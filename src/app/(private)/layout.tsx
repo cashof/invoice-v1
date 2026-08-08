e7f0af5f1cce25a68afa7aba6429bbd8bc8d3b68
Sudais Kawooya <s.kawooya@itms-ug.com>
2026-08-08 13:24:42 +0300

    i think its fixed 2

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,7 +1,5 @@
 import { AppSidebar } from "@/components/app-sidebar";
-import { ChartAreaInteractive } from "@/components/chart-area-interactive";
 
-import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
